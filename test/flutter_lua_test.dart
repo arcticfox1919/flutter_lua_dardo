bca76cf7a2cfc876aab07ba1e8de18f650318481
arcticfox <flutterdev@88.com>
2021-05-10 04:13:28 +0800

    add test

--- test/flutter_lua_test.dart
+++ test/flutter_lua_test.dart
@@ -1,8 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
+
+import 'package:lua_dardo/lua.dart';
 
 
 void main() {
-  test('adds one to input values', () {
-
-  });
+  LuaState ls = newState();
+  ls.openLibs();
+  ls.doFile("test.lua");
 }
